10e41cfd898a5ff8bc400ad282f63d8d2c81d720
musthafaelayadath <musthafaelayadath@gmail.com>
2026-04-08 18:49:49 +0400

    Update app.py

--- app.py
+++ app.py
@@ -61,3 +61,8 @@ def admin():
     return render_template("admin.html", users=users)
 
 app.run(host="0.0.0.0", port=5000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
